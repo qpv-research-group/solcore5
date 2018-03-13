3769b1c7dd0041b2a5ad8d86dfd0f0084b066afc
Diego Alonso-Álvarez <d.alonso-alvarez@imperial.ac.uk>
2018-03-13 14:30:14 +0000

    Added absorption for GaAsP and GaInP, although with just 2 or three-point interpolation

---
+++ solcore/material_data/GaAsP-Material/__init__.py
@@ -0,0 +1,3 @@
+import solcore.science_tracker as st
+
+st.science_reference('GaP refractive index', 'S. Adachi. Optical dispersion relations for GaP, GaAs, GaSb, InP, InAs, InSb, AlxGa1−xAs, and In1−xGaxAsyP1−y, J. Appl. Phys. 66, 6030-6040 (1989)')
---
+++ solcore/material_data/GaInP-Material/__init__.py
@@ -0,0 +1,4 @@
+import solcore.science_tracker as st
+
+st.science_reference('GaP and InP refractive index', 'S. Adachi. Optical dispersion relations for GaP, GaAs, GaSb, InP, InAs, InSb, AlxGa1−xAs, and In1−xGaxAsyP1−y, J. Appl. Phys. 66, 6030-6040 (1989)')
+st.science_reference('GaInP refractive index', 'M. Schubert, V. Gottschalch, C. M. Herzinger, H. Yao, P. G. Snyder and J. A. Woollam. Optical constants of GaxIn1−xP lattice matched to GaAs, J. Appl. Phys. 77, 3416 (1995)')
