9cc38653e7894a0e2c4a1eb08d817129f08ff6ce
phoebe-p <phoebe.pearce15@imperial.ac.uk>
2018-11-05 11:26:16 +0000

    Layer absorption added to RCWA calculator

--- solcore/optics/rcwa.py
+++ solcore/optics/rcwa.py
@@ -64,6 +64,11 @@ def solve_rcwa(solar_cell, options):
     for j in range(len(solar_cell)):
         solar_cell[j].diff_absorption = initial*diff_absorption
         solar_cell[j].absorbed = types.MethodType(absorbed, solar_cell[j])
+        
+        layer_positions = options.position[(options.position >= solar_cell[j].offset) & (
+                options.position < solar_cell[j].offset + solar_cell[j].width)]
+        layer_positions = layer_positions - np.min(layer_positions)
+        solar_cell[j].layer_absorption = np.trapz(solar_cell[j].absorbed(layer_positions), layer_positions, axis=0)
 
     solar_cell.reflected = RAT['R'] * initial
     solar_cell.transmitted = (1 - RAT['R'] - all_absorbed) * initial
