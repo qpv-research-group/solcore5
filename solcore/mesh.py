9b8d55e2658c44845f759aad1cb00c57b69e70c9
Diego <d.alonso-alvarez@imperial.ac.uk>
2020-09-30 10:51:52 +0100

    Add tests to mesh

--- solcore/mesh.py
+++ solcore/mesh.py
@@ -1,81 +1,230 @@
 from __future__ import annotations
 
-from typing import Sequence, Union, Optional
+from typing import Sequence, Optional
+from functools import reduce
 
 import numpy as np
 import xarray as xr
 
 
-class Mesh1D(xr.Dataset):
-    __slots__ = ()
+class NoMeshInformation(Exception):
+    pass
 
-    @classmethod
-    def uniform(cls, npoints: int, nodes: Sequence[float]):
-        """Uniform mesh between the smallest and largest element of the nodes.
 
-        Either the number of points or the spacing can be provided, but not both.
+class InvalidMesh(Exception):
+    pass
 
-        Args:
-            npoints (int): Number of total points in the mesh, including the endpoints.
-            nodes (Sequence[int]): The nodes of the mesh. Only the minimum and
-                maximum values are used in this method.
 
-        Returns:
-            A new Mesh1D object with a uniform mesh.
-        """
-        return cls(
-            points=np.linspace(np.min(nodes), np.max(nodes), npoints, endpoint=True),
-            nodes=nodes,
+def uniform(npoints: int, nodes: Sequence[float]):
+    """Uniform mesh between the smallest and largest element of the nodes.
+
+    Either the number of points or the spacing can be provided, but not both.
+
+    Args:
+        npoints (int): Number of total points in the mesh, including the endpoints.
+        nodes (Sequence[int]): The end points of the mesh.
+
+    Returns:
+        A DataArray object with a uniform mesh.
+    """
+    p = np.linspace(np.min(nodes), np.max(nodes), npoints, endpoint=True)
+    z = xr.DataArray(
+        p,
+        dims=["points"],
+        coords={
+            "interval": ("points", np.ones_like(p).astype(int)),
+            "group": ("points", np.ones_like(p).astype(int)),
+        },
+    )
+    return Mesh(z, nodes)
+
+
+def piecewise_uniform(npoints: Sequence[int], nodes: Sequence[float]):
+    """Uniform mesh within each interval between nodes.
+
+    Args:
+        npoints (Sequence[int]): Iterable providing the number of points per
+            interval. Its length should be one less than the number of nodes.
+        nodes (Sequence[int]): The nodes of the mesh.
+
+    Returns:
+        A new Mesh object with a uniform mesh in each interval.
+    """
+    if len(npoints) != len(nodes) - 1:
+        msg = (
+            "npoints should have more than 1 element and its length be one "
+            f"less than the number of nodes. Found {len(npoints)} and "
+            f"{len(nodes)}"
         )
+        raise ValueError(msg)
 
-    @classmethod
-    def piecewise_uniform(cls, npoints: Sequence[int], nodes: Sequence[float]):
-        """Uniform mesh within each interval between nodes.
+    pieces = []
+    intervals = []
+    for i, n in enumerate(npoints):
+        pieces.append(np.linspace(nodes[i], nodes[i + 1], n))
+        intervals.append(np.full_like(pieces[-1], fill_value=i).astype(int))
+
+    p = np.concatenate(pieces)
+    z = xr.DataArray(
+        p,
+        dims=["points"],
+        coords={
+            "interval": ("points", np.concatenate(intervals)),
+            "group": ("points", np.ones_like(p).astype(int)),
+        },
+    )
+    return Mesh(z=z, nodes=nodes)
+
+
+class Mesh:
+    __slots__ = ("nodes", "z")
+
+    @staticmethod
+    def _validate(z: xr.DataArray) -> None:
+        """Checks that the DataArray has the correct structure for being a Mesh1D.
 
         Args:
-            npoints (Sequence[int]): Iterable providing the number of points per
-                interval. Its length should be one less than the number of nodes.
-                Except for the last interval, the number of points per interval will
-                exclude the endpoint.
-            nodes (Sequence[int]): The nodes of the mesh.
+            z (DataArray): Array to check if it has the correct format.
 
-        Returns:
-            A new Mesh1D object with a uniform mesh in each interval.
+        Raises:
+            InvalidMesh: If the input DataArray has not the correct format.
         """
-        if len(npoints) > 1 and len(npoints) != len(nodes) - 1:
+        if not all(["points" in z.dims, "interval" in z.coords, "group" in z.coords,]):
+            raise InvalidMesh(
+                "The format of the 'z' DataArray is not valid for a Mesh object."
+            )
+
+    def __init__(
+        self, z: xr.DataArray, nodes: Optional[Sequence[float]] = None,
+    ):
+        if nodes is not None and (nodes[0], nodes[-1]) != (z[0].item(), z[-1].item()):
             msg = (
-                "npoints should have more than 1 element and its length be one "
-                f"less than the number of nodes. Found {len(npoints)} and "
-                f"{len(nodes)}"
+                "The first and last nodes must be identical to the first and "
+                f"last points. Found {(nodes[0], nodes[-1])} and "
+                f"{(z[0].item(), z[1].item())}, respectively."
             )
             raise ValueError(msg)
 
-        pieces = []
-        for i, n in enumerate(npoints[:-1]):
-            pieces.append(np.linspace(nodes[i], nodes[i + 1], n, endpoint=False))
-        pieces.append(np.linspace(nodes[-2], nodes[-1], npoints[-1], endpoint=True))
+        self._validate(z)
 
-        return cls(points=np.concatenate(pieces), nodes=nodes,)
+        self.nodes = (
+            np.array(nodes) if nodes is not None else np.array([min(z), max(z)])
+        )
+        self.z = z
 
-    def __init__(
-        self,
-        points: Union[np.ndarray, Sequence],
-        nodes: Optional[Sequence[float]] = None,
-    ):
-        super(Mesh1D, self).__init__()
+    def __repr__(self) -> str:
+        return self.z.__repr__() + "\nNodes:\n" + self.nodes.__repr__()
 
-        self["points"] = xr.DataArray(points, dims="z")
+    @property
+    def ninterval(self):
+        """Number of available intervals."""
+        return len(np.unique(self.z.interval))
 
-        nodes = nodes if nodes is not None else [self.points.min(), self.points.max()]
-        self["nodes"] = xr.DataArray(nodes, dims="z_nodes")
+    @property
+    def ngroup(self):
+        """Number of available groups."""
+        return len(np.unique(self.z.group))
 
-    def offset(self, offset: float) -> Mesh1D:
-        """New Mesh1D obtained by offsetting this one.
+    def select_interval(self, i: int) -> Mesh:
+        """Selects the mesh of the chosen interval.
+
+        The group number of those coordinates is left unchanged. The nodes become the
+        limits of the z values of the interval regardless of being exactly those before.
 
         Args:
-            offset (number): The offset.
+            i (int): The chosen interval to select.
+
+        Raises:
+            ValueError: If the chosen interval does not exist.
 
         Returns:
-            A new Mesh1D object
+            A Mesh with a view the z DataArray containing only the chosen interval.
         """
-        return Mesh1D(self.points.values + offset, self.nodes.values + offset)
+        if i not in self.z.interval:
+            raise ValueError(
+                f"The chosen interval, {i} does not exist. "
+                f"Valid values are {set(self.z.interval)}"
+            )
+
+        return Mesh(z=self.z.where(self.z.interval == i, drop=True))
+
+    def select_group(self, i: int) -> Mesh:
+        """Selects the mesh of the chosen group.
+
+        The group and interval numbers are left unchanged. As the limits of a group
+        always match the limits of an interval and these are values of z by
+        construction, it is guaranteed that the first and last nodes will match the
+        first and last values of z.
+
+        Args:
+            i (int): The chosen group to select.
+
+        Raises:
+            ValueError: If the chosen group does not exist.
+
+        Returns:
+            A Mesh1D with a view the z DataArray containing only the chosen group.
+        """
+        if i not in self.z.group:
+            raise ValueError(
+                f"The chosen interval, {i} does not exist. "
+                f"Valid values are {set(self.z.group)}"
+            )
+
+        z = self.z.where(self.z.group == i, drop=True)
+        nodes = self.nodes[(self.nodes >= min(z)) * (self.nodes <= max(z))]
+        return Mesh(z, nodes)
+
+    def split_in_intervals(self) -> Sequence[Mesh]:
+        """Splits the mesh in one Mesh object per interval.
+
+        Returns:
+            A tuple with one Mesh object for each interval.
+        """
+        return tuple(self.select_interval(i) for i in np.unique(self.z.interval))
+
+    def split_in_groups(self) -> Sequence[Mesh]:
+        """Splits the mesh in one Mesh object per group.
+
+        Returns:
+            A tuple with one Mesh object for each group.
+        """
+        return tuple(self.select_group(i) for i in np.unique(self.z.group))
+
+    def broadcast(self, value, name=""):
+        out = value * xr.ones_like(self.z,)
+        out.name = name
+        out.coords["points"] = self.z
+        return out
+
+
+def concat_mesh(meshes: Sequence[Mesh]) -> Mesh:
+    """Concatenate 2 or more Mesh1D objects."""
+    return reduce(concat2mesh, meshes[1:], meshes[0])
+
+
+def concat2mesh(a: Mesh, b: Mesh) -> Mesh:
+    """Concatenate the two Mesh1D objects."""
+    values = np.concatenate([a.z.values, b.z.values - b.z[0].values + a.z[-1].values])
+
+    intervals = np.concatenate(
+        [a.z.interval.values, b.z.interval.values + a.z.interval.values.max() + 1,]
+    )
+    for i, val in enumerate(np.unique(intervals)):
+        intervals[intervals == val] = i
+
+    groups = np.concatenate(
+        [a.z.group.values, b.z.group.values + a.z.group[:-1].values.max() + 1,]
+    )
+    for i, val in enumerate(np.unique(groups)):
+        groups[groups == val] = i
+
+    nodes = np.unique(np.concatenate([a.nodes, b.nodes - b.nodes[0] + a.nodes[-1]]))
+
+    z = xr.DataArray(
+        values,
+        dims=["points"],
+        name="z",
+        coords={"interval": ("points", intervals), "group": ("points", groups),},
+    )
+    return Mesh(z, nodes)
---
+++ tests/test_mesh.py
@@ -0,0 +1,184 @@
+from pytest import raises
+
+
+def test_validate():
+    import xarray as xr
+    import numpy as np
+    from solcore.mesh import InvalidMesh, Mesh
+
+    p = [1, 2, 3]
+
+    # points not a dimension name
+    z = xr.DataArray(
+        p,
+        dims=["y"],
+        coords={
+            "interval": ("y", np.ones_like(p).astype(int)),
+            "group": ("y", np.ones_like(p).astype(int)),
+        },
+    )
+    with raises(InvalidMesh):
+        Mesh._validate(z)
+
+    # No interval information
+    z = xr.DataArray(
+        p, dims=["points"], coords={"group": ("points", np.ones_like(p).astype(int)),},
+    )
+    with raises(InvalidMesh):
+        Mesh._validate(z)
+
+    # No group information
+    z = xr.DataArray(
+        p,
+        dims=["points"],
+        coords={"interval": ("points", np.ones_like(p).astype(int)),},
+    )
+    with raises(InvalidMesh):
+        Mesh._validate(z)
+
+    # All OK
+    z = xr.DataArray(
+        p,
+        dims=["points"],
+        coords={
+            "interval": ("points", np.ones_like(p).astype(int)),
+            "group": ("points", np.ones_like(p).astype(int)),
+        },
+    )
+    Mesh._validate(z)
+
+
+def test_init_mesh():
+    from solcore.mesh import Mesh
+    import xarray as xr
+    import numpy as np
+
+    p = [1, 2, 3]
+    z = xr.DataArray(
+        p,
+        dims=["points"],
+        coords={
+            "interval": ("points", np.ones_like(p).astype(int)),
+            "group": ("points", np.ones_like(p).astype(int)),
+        },
+    )
+    with raises(ValueError):
+        Mesh(z, [0, 10])
+
+    mesh = Mesh(z)
+    assert all(mesh.nodes == [z[0].item(), z[-1].item()])
+
+
+def test_uniform():
+    from solcore.mesh import uniform
+
+    nodes = [4, 40, 400]
+    mesh = uniform(42, nodes)
+
+    assert min(nodes) == min(mesh.z).item()
+    assert max(nodes) == max(mesh.z).item()
+
+
+def test_piecewise_uniform():
+    from solcore.mesh import piecewise_uniform
+
+    nodes = [4, 40, 400]
+
+    # Insufficient number of points -> need to be equal to number of nodes minus 1
+    with raises(ValueError):
+        piecewise_uniform([42], nodes)
+
+    npoints = [42, 100]
+    mesh = piecewise_uniform(npoints, nodes)
+    assert all(n in mesh.z for n in nodes)
+    assert len(mesh.z) == sum(npoints)
+    assert mesh.ninterval == 2
+    assert mesh.ngroup == 1
+    assert sum(mesh.z == nodes[1]) == 2
+
+
+def test_select_interval():
+    from solcore.mesh import piecewise_uniform
+
+    nodes = [4, 40, 400]
+    npoints = [42, 100]
+    mesh = piecewise_uniform(npoints, nodes)
+
+    interval = mesh.select_interval(0)
+    assert interval.ninterval == 1
+    assert len(interval.z) == npoints[0]
+    assert all(interval.z.interval == 0)
+    assert all(interval.nodes == nodes[:2])
+
+    interval = mesh.select_interval(1)
+    assert interval.ninterval == 1
+    assert len(interval.z) == npoints[1]
+    assert all(interval.z.interval == 1)
+    assert all(interval.nodes == nodes[1:])
+
+
+def test_split_in_intervals():
+    from solcore.mesh import piecewise_uniform
+
+    nodes = [4, 40, 400, 4000]
+    npoints = [42, 100, 200]
+    mesh = piecewise_uniform(npoints, nodes)
+
+    intervals = mesh.split_in_intervals()
+    for i, interval in enumerate(intervals):
+        assert interval.ninterval == 1
+        assert len(interval.z) == npoints[i]
+        assert all(interval.z.interval == i)
+        assert all(interval.nodes == nodes[i : i + 2])
+
+
+def test_concat2mesh():
+    from solcore.mesh import uniform, piecewise_uniform, concat2mesh
+    import numpy as np
+
+    nodes1 = [4, 40, 400]
+    mesh1 = piecewise_uniform([4, 40], nodes1)
+
+    nodes2 = [4, 20]
+    mesh2 = uniform(100, nodes2)
+
+    mesh = concat2mesh(mesh1, mesh2)
+    assert all(np.unique(mesh.z.interval) == (0, 1, 2))
+    assert all(np.unique(mesh.z.group) == (0, 1))
+    assert all(mesh.nodes == [4, 40, 400, nodes1[-1] + nodes2[-1] - nodes2[0]])
+    assert len(mesh.z) == len(mesh1.z) + len(mesh2.z)
+    assert max(mesh.z) == max(mesh1.z) + max(mesh2.z) - min(mesh2.z)
+
+
+def test_concat_mesh():
+    from solcore.mesh import piecewise_uniform, concat_mesh
+    import numpy as np
+    from functools import reduce
+
+    rng = np.random.default_rng()
+    int_per_mesh = rng.integers(2, 5)
+    num_meshes = rng.integers(3, 7)
+
+    all_nodes = []
+    all_meshes = []
+
+    for i in range(num_meshes):
+        all_nodes.append(np.cumsum(rng.integers(5, 50, int_per_mesh + 1)))
+        npoints = rng.integers(10, 100, int_per_mesh)
+        all_meshes.append(piecewise_uniform(npoints, all_nodes[-1]))
+
+    mesh = concat_mesh(all_meshes)
+    assert all(np.unique(mesh.z.interval) == tuple(range(num_meshes * int_per_mesh)))
+    assert all(np.unique(mesh.z.group) == tuple(range(num_meshes)))
+    assert len(mesh.z) == reduce(lambda a, b: a + len(b.z), all_meshes, 0)
+    assert all(
+        mesh.nodes
+        == list(
+            reduce(
+                lambda a, b: a + (b[1:] - b[0] + a[-1]).tolist(),
+                all_nodes[1:],
+                all_nodes[0].tolist(),
+            )
+        )
+    )
+
