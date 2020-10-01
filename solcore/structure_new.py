b527d03256cefc83b900d81fba49e8a5ea385b38
Diego <d.alonso-alvarez@imperial.ac.uk>
2020-10-01 21:59:55 +0100

    :sparkles:Add from dict/dataframe methods to Structure

--- solcore/structure_new.py
+++ solcore/structure_new.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import Optional, Sequence, Tuple, Hashable, Union
 from dataclasses import dataclass
+from itertools import chain
 
 import pandas as pd
 
@@ -81,7 +82,7 @@ class Layer:
             A dictionary with all the layer information.
         """
         result = dict(width=self.width, geometry=self.geometry)
-        result.update(self.material.to_dict())
+        result.update(self.material.to_dict() if self.material is not None else {})
         return result
 
     def to_dataframe(self, include_nk=False) -> pd.DataFrame:
@@ -107,16 +108,77 @@ class Layer:
 
 
 class Structure(Tuple[Layer, ...]):
+    @classmethod
+    def from_records(cls, data: Sequence[dict]) -> Structure:
+        """Construct a Structure object from a sequence of dictionaries.
+
+        Args:
+            data (Sequence[dict]): A sequence of dictionaries. Each dictionary must be
+            valid input for Layer.from_dict, which is called internally
+
+        Returns:
+            A new Structure object.
+        """
+        return cls([Layer.from_dict(d) for d in data])
+
+    @classmethod
+    def from_dataframe(
+        cls,
+        data: pd.DataFrame,
+        nk_cols: Union[Sequence[Tuple[str, str]], bool] = False,
+    ) -> Structure:
+        """Construct a Structure object from a pandas DataFrame.
+
+        Internally, it calls Layer.from_dataframe method.
+
+        Args:
+            data (pd.DataFrame): A DataFrame with all the structure information.
+            nk_cols (Union[Sequence[Tuple[str, str]], bool]):
+                - if False, no nk data is retrieved from the dataframe.
+                - if True, the column names are guessed from the material string name
+                    of each layer and data attempted to be retrieved.
+                - if a Sequence of tuples, each tuple must have the name of the columns
+                    containing the wavelength and the nk data, respectively. In this
+                    case, there must be as many tuples as number of layers in the
+                    Structure.
+
+        Returns:
+            A new Structure object.
+        """
+        if not nk_cols:
+            return cls([Layer.from_dataframe(data, index=i) for i in data.index])
+        elif isinstance(nk_cols, tuple):
+            cols: list = list(nk_cols)
+        else:
+            cols = []
+            for i in data.index:
+                name = data.loc[i, "name"]
+                composition = data.loc[i, "composition"]
+                for k, v in composition.items():
+                    name = name.replace(k, f"{k}{v:.2}")
+                cols.append((f"wavelength {name}", f"nk {name}"))
+
+        all_nk_cols = tuple(chain.from_iterable(cols))
+        d = data.drop(all_nk_cols)
+        return cls(
+            [
+                Layer.from_dataframe(
+                    pd.concat([d, data[cols[i]]], axis=1), index=i, nk_cols=cols[i],
+                )
+                for i in d.dropna(how="all").index
+            ]
+        )
+
     def __new__(cls, layers: Sequence[Layer], name: str = "No name"):
         if not all(isinstance(layer, Layer) for layer in layers):
             raise TypeError("Layers must be of type Layer.")
-        return super(Structure, cls).__new__(cls, tuple(layers))
+        return super(Structure, cls).__new__(cls, tuple(layers))  # type: ignore
 
     def __init__(self, layers: Sequence[Layer], name: str = "No name"):
         self.name = name
 
     @property
-    def widths(self) -> Tuple[float]:
+    def widths(self) -> Tuple[float, ...]:
         return tuple(layer.width for layer in self)
 
     @property
@@ -124,7 +186,7 @@ class Structure(Tuple[Layer, ...]):
         return sum(self.widths)
 
     @property
-    def relative_widths(self) -> Tuple[float]:
+    def relative_widths(self) -> Tuple[float, ...]:
         return tuple(layer.width / self.width for layer in self)
 
     def insert_layer(
