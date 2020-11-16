4cc067b1b05defc8dbbe6cd9170a37fea630f6b8
Diego <d.alonso-alvarez@imperial.ac.uk>
2020-11-16 10:03:14 +0000

    Transoform constants into quantities

--- solcore/constants.py
+++ solcore/constants.py
@@ -1,11 +1,35 @@
-kb=1.3806503e-23
-q=1.60217646e-19
-h=6.626068e-34
-hbar=1.05457148e-34
-electron_mass=9.10938188e-31
-vacuum_permittivity=8.854187817e-12
-c=299792458
-fs=6.8e-5
-Ts=5762.0
-solar_constant=1352.89684374
-pi=3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+from pint import Quantity
+import numpy as np
+
+pi = Quantity(np.pi, "dimensionless")
+"""Ehmmm... the Pi number"""
+
+q = Quantity(1.60217646e-19, "C")
+"""Electron charge"""
+
+kb = Quantity(1.3806503e-23, "J/K")
+"""Boltzmann's constant"""
+
+h = Quantity(6.626068e-34, "J*s")
+"""Plank's constant"""
+
+hbar = h / (2 * pi)
+"""Reduced Plank's constant"""
+
+electron_mass = Quantity(9.10938188e-31, "kg")
+"""Electron rest mass"""
+
+vacuum_permittivity = Quantity(8.854187817e-12, "F/m")
+"""Permitivity of the vacuum"""
+
+c = Quantity(299792458.0, "m/s")
+"""Speed of light"""
+
+fs = Quantity(6.8e-5, "steradian")
+"""Solid angle of the Sun - or entendu"""
+
+Ts = Quantity(5762.0, "k")
+"""Temperature of the Sun when considered as a black body"""
+
+solar_constant = Quantity(1361.0, "kW/m**2")
+"""Solar constant - density of solar irradiance out of the atmosphere"""
