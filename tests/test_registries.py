1b6fa61c4e883f45e8bb203628275894a9a90fe2
Diego <d.alonso-alvarez@imperial.ac.uk>
2022-10-20 11:40:40 +0100

    Fix test that was testing the wrong thing

--- tests/test_registries.py
+++ tests/test_registries.py
@@ -13,11 +13,11 @@ def test_register_action():
     with pytest.raises(ValueError):
 
         @registries.register_action("pre-process")
-        def custom_solve_optics(*args, **kwargs):
+        def custom_pre_process_cell(*args, **kwargs):
             pass
 
-    @registries.register_action("optics", overwrite=True)
-    def custom_solve_optics(*args, **kwargs):
+    @registries.register_action("pre-process", overwrite=True)
+    def another_pre_process_cell(*args, **kwargs):
         pass
 
-    assert registries.ACTIONS_REGISTRY["optics"] == custom_solve_optics
+    assert registries.ACTIONS_REGISTRY["pre-process"] == another_pre_process_cell
