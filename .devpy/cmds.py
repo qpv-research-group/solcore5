427b2dbb183dd907b9889833b746da457c1a867b
Luigi <luigi3000@gmail.com>
2022-12-08 12:30:32 +0100

    Created a new devpy command to install the package dependencies with the possibility to install the test dep and doc dep

--- .devpy/cmds.py
+++ .devpy/cmds.py
@@ -1,7 +1,35 @@
 import click
-import sys
 from devpy import util
 from devpy.cmds.util import get_site_packages, set_pythonpath, run
+
+
+@click.command()
+@click.option(
+    "-test-dep", is_flag=True, help="If to install test dependecies"
+)
+@click.option(
+    "-doc-dep", is_flag=True, help="If to install test dependecies"
+)
+def install_dependencies(test_dep=False, doc_dep=False):
+    """Command that accesses `pyproject.toml` configuration"""
+    config = util.get_config()
+    default_dependencies = config["project"]['dependencies']
+    print("Installing dependencies", default_dependencies)
+    run(
+        ["pip", "install"] + list(default_dependencies),
+    )
+    if test_dep:
+        test_dependencies = config["project.optional-dependencies"]['test']
+        print("Installing test-dependencies", config["project.optional-dependencies"]['test'])
+        run(
+            ["pip", "install"] + list(test_dependencies),
+        )
+    if doc_dep:
+        doc_dependencies = config["project.optional-dependencies"]['doc']
+        print("Installing doc-dependencies", doc_dependencies)
+        run(
+            ["pip", "install"] + list(doc_dependencies),
+        )
 
 
 @click.command()
