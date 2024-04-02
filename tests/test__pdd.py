fe25759c2ace55cf7c811ddf2d47601b5f2774a4
Phoebe Pearce <p.pearce@unsw.edu.au>
2024-04-02 17:12:46 +0200

    more test fixes

--- tests/test__pdd.py
+++ tests/test__pdd.py
@@ -140,13 +140,15 @@ def test_calculate_iv(mocker):
     # And with illumination
     mock_eq.reset_mock()
     mock_sc.reset_mock()
-    out = calculate_iv(junction, vlimit, vstep, light_iv=True, output_iv=output_iv)
+    out = calculate_iv(junction, vlimit, vstep, light_iv=True, output_iv=output_iv,
+                       T=T_in)
     mock_eq.assert_not_called()
     mock_sc.assert_called()
 
     # And if the vlimit is negative, so it will be the step, internally
-    out = calculate_iv(junction, -vlimit, vstep, light_iv=True, output_iv=output_iv)
-    mock_ddModel.runiv.assert_called_with(-vlimit, -vstep, output_iv, 0)
+    out = calculate_iv(junction, -vlimit, vstep, light_iv=True, output_iv=output_iv,
+                       T=T_in)
+    mock_ddModel.runiv.assert_called_with(-vlimit, -vstep, T_in, output_iv, 0)
 
 
 def test_find_minimum_bandgap():
